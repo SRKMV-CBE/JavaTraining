a517313b4779a58ebfc884889fba731d6051dd32
Venkateshwaran Lakshmi Narayanan <lvenkteshwaran9@gmail.com>
2023-07-05 12:36:41 +0530

    Assigment Java Program

---
+++ Assignments/21uit022_Venkatshwaran/Decimal2Octal.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Decimal2Octal
+{
+	public static void main(String arg[])
+	{
+		System.out.println("Enter Any Decimal Number:");
+		Scanner input = new Scanner(System.in);
+		int n =input.nextInt();
+		int rem;
+		int dec=n;
+		String bin="";
+		while(n>0)
+		{
+			rem=n%8;
+			bin=rem+bin;
+			n=n/8;
+		}
+		System.out.println("The Given Decimal Number"+" "+dec+" "+"Converted into Octal Number of Is:"+bin);
+	}
+}
