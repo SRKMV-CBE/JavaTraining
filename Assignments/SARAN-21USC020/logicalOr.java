6a803d9e8e01f8d4e4f89751bb2cd5e10e89ca57
SaranKavin <138579247+SaranKavin@users.noreply.github.com>
2023-07-06 01:15:01 +0530

    Add files via upload

---
+++ Assignments/SARAN-21USC020/logicalOr.java
@@ -0,0 +1,20 @@
+//Logical OR gate (Hard Code)
+
+public class LogicalOr
+{
+        public static void main(String[] args)
+    {
+        String s1 = "true";
+        String s2 = "true";
+        boolean result = logicalAnd(s1, s2);
+        System.out.println("Logical AND: " + result);
+    }
+
+        public static boolean logicalAnd(String s1, String s2)
+    {
+        boolean b1 = Boolean.parseBoolean(s1);
+        boolean b2 = Boolean.parseBoolean(s2);
+        return b1 || b2;
+    }
+}
+
