84bd9d01b6e282b7a94627359517b87de9c03ccc
VIJAYWHAT <vijayrv042@gmail.com>
2023-07-05 06:51:35 +0530

    Assignments Added

---
+++ Assignments/23UCS046-Vijay/OddOrEven.java
@@ -0,0 +1,14 @@
+public class OddOrEven{
+
+    public static void main(String... agrs){
+
+        int num = 14;
+        if(num % 2 == 0){
+            System.out.println("Even Number");
+        }
+        else{
+            System.out.println("Odd Number");
+        }
+
+    }
+}
