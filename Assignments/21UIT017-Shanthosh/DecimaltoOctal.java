b02515e2efa42b3d8e04a3edbf130ea14a772aac
Shanthosh834 <138580302+Shanthosh834@users.noreply.github.com>
2023-07-06 10:04:21 +0530

    Update DecimaltoOctal.java

--- Assignments/21UIT017-Shanthosh/DecimaltoOctal.java
+++ Assignments/21UIT017-Shanthosh/DecimaltoOctal.java
@@ -1,1 +1,16 @@
+public class DecimaltoBinary
+        {
+     public static void main(String[] args)
+                {
+                        System.out.println("The decimal no of 9 ");
+ int n=9,add=0,r;
+  while(n>0)
+     {
+          r = n%8;
+          add = add * 10 + r;
+           n = n/8;
+     }
+System.out.println("The octal number of num 9 :" +add);
+                }
+        }
 
