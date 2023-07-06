1e3141d6227f62a2a6d156345c8f52369512c08e
AmeetKumar <ameetkumar2002@gmail>
2023-07-06 09:25:09 +0530

    AmeetKumar Assignment

---
+++ Assignments/AmeetKumar-21UCA002/factorial.java
@@ -0,0 +1,16 @@
+/* java program to find factorial */
+public class Factorial
+{
+	public static void main(String[] args)
+	{
+		int num=5;
+		int f=1;
+		for(int i=1;i<=num;i++)
+		{
+			f*=i;
+			
+		}
+		System.out.println("The Factorial is:"+f);
+	}
+}
+
