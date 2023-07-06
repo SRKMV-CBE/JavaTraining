d89cff553f0c285b1f5c87db53bcb876fa8a0a17
YOGESHWARAN PALPANDI <yogeshpalpandiyan@gmail.com>
2023-07-06 11:45:28 +0530

    decimal code

---
+++ Assignments/21USC031-YOGESHWARAN/DecimalToBinary.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class DecimalToBinary
+{
+	public static void main(String arg[])
+	{
+		System.out.println("Enter Any Decimal Number:");
+		Scanner input = new Scanner(System.in);
+		int n =input.nextInt();
+		int rem;
+		int dec=n;
+		String bin="";
+		while(n>0)
+		{
+			rem=n%2;
+			bin=rem+bin;
+			n=n/2;
+		}
+		System.out.println("The Given Decimal Number"+" "+dec+" "+"Converted into Binary Number of Is:"+bin);
+	}
+}
