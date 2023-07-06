aad80c3129174a49f9a5f76604bf4851f6f85d8d
Jegathesh kalimuthu <Jegatheshjack93@gmail.com>
2023-07-06 09:50:15 +0530

    Factorial program

--- Assignments/21UIT006-Jegathes/Factorial.java
+++ Assignments/21UIT006-Jegathes/Factorial.java
@@ -1,10 +1,10 @@
 class Factorial{
 	public static void main(String args[]){
-		int n=6;
-		int mul=1;
-		for(int i=1;i<=n;i++){
-		mul*=i;
+		int p=6;
+		int y=1;
+		for(int i=1;i<=p;i++){
+		y*=i;
 		}
-		System.out.println("The Factorial Of "+n+" Is : "+mul);
+		System.out.println("The Factorial Of "+p+" Is : "+y);
 	}
 }
