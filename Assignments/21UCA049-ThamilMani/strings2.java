0a04f1d7c6abdbd2fc4f4fee721210522dbc4a3e
THAMILMANI VARATHARAJAN <v.kishoremani@gmail.com>
2023-07-08 13:37:24 +0530

    ThamilMani-delete String2.java

--- Assignments/21UCA049-ThamilMani/strings2.java
+++
@@ -1,9 +0,0 @@
-import java.lang.String;
-class strings2{
-    public static void main(String args[]){
-        String s1="2";
-        String s2="3";
-        String sum=s1+s2;
-        System.out.println("String : "+sum);
-    }
-}
