9f45b67fa19fbb79c56227c0c70c549663727596
THAMILMANI VARATHARAJAN <v.kishoremani@gmail.com>
2023-07-08 13:31:19 +0530

    ThamilMani-Class Student Contain Attributes and Behaviors.Class TestStudent Contain Main()

---
+++ Assignments/21UCA049-ThamilMani/CmdLineArgsInput.java
@@ -0,0 +1,8 @@
+class CmdLineArgsInput{
+	public static void main(String args[]){
+		int a=Integer.parseInt(args[0]);
+		int b=Integer.parseInt(args[1]);
+		int c=a+b;
+		System.out.println("The Sum Of A And B : "+c);		
+	}
+}
