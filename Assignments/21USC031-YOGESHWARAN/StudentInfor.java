fafd55bd673025d2ae398e6b65cdca8e65854bc8
YOGESHWARAN PALPANDI <yogeshpalpandiyan@gmail.com>
2023-07-11 13:08:49 +0530

    21USC031-Student Information Hardcore Program

---
+++ Assignments/21USC031-YOGESHWARAN/StudentInfor.java
@@ -0,0 +1,18 @@
+public class StudentInfor
+{
+	String name;
+	int age;
+
+	public static void main(String... args)
+	{
+		StudentInfor s1=new StudentInfor();
+
+		s1.name="Yogesh";
+		s1.age=19;
+
+		System.out.println("Name :"+s1.name);
+
+		System.out.println("Age :"+s1.age);
+		
+	}
+}
